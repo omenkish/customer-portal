46b8a0a625fa7625cdfb483f2665aa833fe52505
Omenkish <omenkish@gmail.com>
2020-06-06 19:05:03 +0100

    Initial setup and installed basic gems

---
+++ db/migrate/20200606175824_create_tickets.rb
@@ -0,0 +1,11 @@
+class CreateTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.text :description
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
